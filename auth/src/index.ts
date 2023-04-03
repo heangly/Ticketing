c89866c191ba779f673c59262f4a85bc1ff139f8
Heang Ly <heanglypa@gmail.com>
2023-04-03 11:29:22 -0400

    finish error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signupRouter } from './routes/signup'
 import { signoutRouter } from './routes/signout'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -14,6 +16,14 @@ app.use(signinRouter)
 app.use(signupRouter)
 app.use(signoutRouter)
 
+// any undefined routes
+app.all('*', async () => {
+  // Synchronous code work with no extra work. Throw new error is enough
+  // Async code will need next() OR we can use NPM package (express-async-errors) to change default behavior of Express, using (express-async-errors) you can just throw error (no need async)
+  throw new NotFoundError()
+})
+
+// any error thatis thrown will be catched here
 app.use(errorHandler)
 
 app.listen(3000, () => console.log('==> Auth Sever is running on PORT 3000 <=='))
