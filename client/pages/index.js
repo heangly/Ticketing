42c20c871963031f90b8c81cc5f091b6ad2a5420
Heang Ly <heanglypa@gmail.com>
2023-06-22 08:41:29 -0400

    getStaticProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,20 @@
-const index = () => {
-  return <div>index</div>
+import axios from 'axios'
+
+// render each component with pre fetch data ONE TIME
+// This function will be execute on Server, so this code wont show up on client side
+export const getStaticProps = async () => {
+  try {
+    const response = await axios.get('/api/users/currentuser')
+    return { props: { currentUser: response.data } }
+  } catch (error) {
+    console.log(error.message)
+    return { props: { currentUser: null } }
+  }
 }
 
-export default index
+const LandingPage = ({ currentUser }) => {
+  console.log(`I am on component, ${currentUser}`)
+  return <div>LandingPage</div>
+}
+
+export default LandingPage
